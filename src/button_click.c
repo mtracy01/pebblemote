3f18a40cb0ff34e53e1362dac7afac85cc9372af
Matthew David Tracy <matthetracy@gmail.com>
2015-04-30 13:31:29 -0400

    Added communication between the pebble and source
    Still need to add functionality to handle callbacks and fix pebble bugs.

--- src/button_click.c
+++ src/button_click.c
@@ -1,23 +1,34 @@
 #include <pebble.h>
 
+#define KEY_DATA 5
+
 static Window *window;
 static TextLayer *text_forward;   /* Top    Layer   */
 static TextLayer *text_play;     /*  Middle Layer  */
 static TextLayer *text_back;    /*   Bottom Layer */
 
 //Handle play request
+//static void sendMessage(int num);
+static void send_int(int key, int value);
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   //text_layer_set_text(text_layer, "Select");
+  send_int(1,1);
+  
 }
 
 //Handle forward request
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   //text_layer_set_text(text_layer, "Up");
+  //sendMessage(0);
+  send_int(1,1);
 }
 
 //Handle back request
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   //text_layer_set_text(text_layer, "Down");
+  //sendMessage(2);
+  send_int(1,1);
 }
 
 static void click_config_provider(void *context) {
@@ -37,9 +48,9 @@ static void window_load(Window *window) {
   text_layer_set_text(text_play, "Play/Pause");
   text_layer_set_text(text_back, "Back");
   
-  text_layer_set_text_alignment(text_forward, GAlignTop);
+ // text_layer_set_text_alignment(text_forward, GAlignTop);
   text_layer_set_text_alignment(text_play, GTextAlignmentCenter);
-  text_layer_set_text_alignment(text_back, GAlignTop);
+ // text_layer_set_text_alignment(text_back, GAlignTop);
   /*text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
@@ -52,7 +63,49 @@ static void window_unload(Window *window) {
   text_layer_destroy(text_forward);
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+
+  // Process all pairs present
+  while (t != NULL) {
+    // Long lived buffer
+    static char s_buffer[64];
+
+    // Process this pair's key
+    switch (t->key) {
+      case KEY_DATA:
+        // Copy value and display
+        snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", t->value->cstring);
+        //text_layer_set_text(s_output_layer, s_buffer);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+
 static void init(void) {
+  //Set appropriate callback handlers
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
@@ -66,6 +119,30 @@ static void init(void) {
 static void deinit(void) {
   window_destroy(window);
 }
+
+static void send_int(int key, int value) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_int(iter, key, &value, sizeof(int), true);
+  app_message_outbox_send();
+}
+/*static void sendMessage(int num){
+  //Send the message through a dictionary (as required by Pebble)
+  DictionaryIterator* iter;
+  app_message_outbox_begin(&iter);
+  APP_LOG(APP_LOG_LEVEL_INFO,"GOT HERE");
+  //Create the key value pair storing our data
+  int key=1;
+  int com=num;
+  APP_LOG(APP_LOG_LEVEL_INFO,"GOT HERE");
+  dict_write_int(iter,key,&com,sizeof(int),true);
+  //Send the message
+ APP_LOG(APP_LOG_LEVEL_INFO,"GOT HERE");
+  app_message_outbox_send();
+  APP_LOG(APP_LOG_LEVEL_INFO,"GOT HERE");
+}*/
+
+
 
 int main(void) {
   init();
