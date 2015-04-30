061ea2dd0b8f39eb914ca44c804e5f02eefad430
Matthew David Tracy <matthetracy@gmail.com>
2015-04-29 20:24:09 -0400

    Added some test buttons

--- src/button_click.c
+++ src/button_click.c
@@ -1,18 +1,23 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *text_forward;   /* Top    Layer   */
+static TextLayer *text_play;     /*  Middle Layer  */
+static TextLayer *text_back;    /*   Bottom Layer */
 
+//Handle play request
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  //text_layer_set_text(text_layer, "Select");
 }
 
+//Handle forward request
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  //text_layer_set_text(text_layer, "Up");
 }
 
+//Handle back request
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  //text_layer_set_text(text_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -24,15 +29,27 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_forward =  text_layer_create((GRect){.origin = {0,72}, .size = { bounds.size.w, 20}});
+  text_play    =  text_layer_create((GRect){.origin = {0,72}, .size = { bounds.size.w, 20}});
+  text_back    =  text_layer_create((GRect){.origin = {0,72}, .size = { bounds.size.w, 20}});
+ 
+  text_layer_set_text(text_forward, "Forward");
+  text_layer_set_text(text_play, "Play/Pause");
+  text_layer_set_text(text_back, "Back");
+  
+  text_layer_set_text_alignment(text_forward, GAlignTop);
+  text_layer_set_text_alignment(text_play, GTextAlignmentCenter);
+  text_layer_set_text_alignment(text_back, GAlignTop);
+  /*text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  */
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(text_play);
+  text_layer_destroy(text_back);
+  text_layer_destroy(text_forward);
 }
 
 static void init(void) {
